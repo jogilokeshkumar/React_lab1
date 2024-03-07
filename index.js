9bb427c5d514a79545ba3c81838e7e713f6aa17d
JOGI LOKESH KUMAR <jogilokeshkumar03@gmail.com>
2024-03-08 01:55:16 +0530

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import RouterHandler from './routes/router';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <RouterHandler />
-);
